c5a76de2037ac47c19eab4bfc9dc139b86fee2ae
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-05-29 15:48:10 -0700

    created REPL for commands and linked up the player and room classes

--- src/adv.py
+++ src/adv.py
@@ -1,4 +1,5 @@
 from room import Room
+from player import Player
 
 # Declare all the rooms
 
@@ -38,14 +39,22 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+# Ask for player's name
+player_name = input("What's your character's name?")
+player = Player(player_name)
+print(f"Welcome {player}\n")
+
 
 # Write a loop that:
-#
-# * Prints the current room name
-# * Prints the current description (the textwrap module might be useful here).
-# * Waits for user input and decides what to do.
-#
+while True:
+    # * Prints the current room name
+    print(room['outside'])
+    # * Prints the current description (the textwrap module might be useful here).
+    # * Waits for user input and decides what to do.
+
+    cmd = input("\nWhat would you like to do?\n")
 # If the user enters a cardinal direction, attempt to move to the room there.
 # Print an error message if the movement isn't allowed.
-#
+    if cmd == "q":
+        break
 # If the user enters "q", quit the game.
